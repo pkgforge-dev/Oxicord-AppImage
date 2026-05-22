a818f1c650a31a67731282ea19b156a347e7278b
Link4Electronics <link4electronics@gmail.com>
2026-05-22 07:10:19 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,24 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm chafa tmux
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+mkdir -p ./AppDir/bin
+TAG=$(wget -qO- https://api.github.com/repos/linuxmobile/oxicord/releases/latest \
+      | sed -n 's/.*"tag_name": *"\([^"]*\)".*/\1/p' | head -1)
+VERSION=${TAG#v}
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+LINK=$(wget -qO- https://api.github.com/repos/linuxmobile/oxicord/releases/latest \
+      | grep -o "https://[^\" ]*$ARCH-unknown-linux-gnu")
+if ! wget --retry-connrefused --tries=30 "$LINK" -O ./AppDir/bin/oxicord 2>/tmp/download.log; then
+    cat /tmp/download.log
+    exit 1
+fi
+chmod +x ./AppDir/bin/oxicord
