1c0d70cc9435f354d3fe47a2997686e05e49ee89
Link4Electronics <link4electronics@gmail.com>
2026-09-10 01:33:25 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,12 +21,11 @@ get-debloated-pkgs --add-common --prefer-nano
 echo "Getting app..."
 echo "---------------------------------------------------------------"
 
+mkdir -p ./AppDir/bin
 LINK=$(wget https://api.github.com/repos/linuxmobile/oxicord/releases -O - \
       | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*$ARCH")
 echo "$LINK" | awk -F'/' '{gsub(/^v/, "", $(NF-1)); print $(NF-1); exit}' > ~/version
-if ! wget --retry-connrefused --tries=30 "$DEB_LINK" -O /tmp/app 2>/tmp/download.log; then
+if ! wget --retry-connrefused --tries=30 "$DEB_LINK" -O ./AppDir/bin/oxicord 2>/tmp/download.log; then
 	cat /tmp/download.log
 	exit 1
 fi
-
-mkdir -p ./AppDir/bin
